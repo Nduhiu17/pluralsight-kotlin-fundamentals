e81a0050dbc1e989d2a306b4ebf87b5351f990b9
KamauBrian <brian.kamau@cellulant.com>
2020-01-07 09:56:13 +0300

    feat: update high order functions

--- src/io/kotlin/fundamentals/HighOrderFunctions.kt
+++ src/io/kotlin/fundamentals/HighOrderFunctions.kt
@@ -1,2 +1,14 @@
 package io.kotlin.fundamentals
 
+val action = {println("Hello, World")}
+
+val calculation: (Int,Int) -> Int = {x, y -> x *y}
+
+fun doSomething(func: () -> Unit){
+    func()
+}
+
+
+fun main(args: Array<String>) {
+    doSomething(action)
+}
